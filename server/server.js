15e654c007b147ad762555274628ab9f86aac787
Edgar <elindo586@gmail.com>
2022-04-07 20:25:39 -0400

    remove network restrictions

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,18 +14,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // Use this to log mongo queries being executed!
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
-if( process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);  
+app.use(routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 });
